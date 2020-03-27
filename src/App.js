222e291a1e02a6710b32409c5c28b034046f9e8c
Danny North <dnorth@adobe.com>
2020-03-27 16:36:52 -0600

    DCMA Takedown notice

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://avatars1.githubusercontent.com/u/9939603?s=400&u=92b81cbfcddae7e57182a54940616f399d61e30e&v=4" className="App-logo" alt="logo" />
+        <img src="https://avatars1.githubusercontent.com/u/9939603?s=400&u=92b81cbfcddae7e57182a54940616f399d61e30e&v=4" className="App-logo DCMATAKEDOWNBLUR" alt="logo" />
         <p>
-          Thanks Adrian! I'm on Netlify!
+          Due to a dispute from the owner of the image, it has been blurred for legal purposes.
+        </p>
+        <p>
+        (Thanks for nothing, Adrian...)
         </p>
       </header>
     </div>
